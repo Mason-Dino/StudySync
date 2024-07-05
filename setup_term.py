0f697e4efa095ebb1c70ab2611c73e072baef315
LegosAndStuff <masonnorberg@midco.net>
2024-07-05 17:35:16 -0500

    add theme

--- setup_term.py
+++ setup_term.py
@@ -3,10 +3,25 @@ import json
 
 print("Welcome to Assignment Tracker!")
 print("Today we are going to set up your experience!")
-print("\n")
+print("")
 
 setup = {}
 
 mode = input("What mode would you like (dark or light): ")
 setup["mode"] = mode
+
+print(
+    """Themes Available:
+    \tBlue
+    \tGreen
+    \tOrange
+    \tPurple
+    \tREd
+    \tTeal
+    \tYellow
+    \tGrey
+    """)
+theme = input("Please choose a theme from above: ")
+
+setup["theme"] = theme
 

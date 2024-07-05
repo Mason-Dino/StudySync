0e4e879bafa04a02b795ff26dcf4ff46d5fc0ed9
LegosAndStuff <masonnorberg@midco.net>
2024-07-05 17:39:18 -0500

    asked for classes

--- setup_term.py
+++ setup_term.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from id import makeID
 import json
 
 print("Welcome to Assignment Tracker!")
@@ -22,6 +23,15 @@ print(
     \tGrey
     """)
 theme = input("Please choose a theme from above: ")
-
 setup["theme"] = theme
 
+numClasses = int(input("How many classes do you have (max 10): "))
+
+if numClasses > 10:
+    print(f"Because you put {numClasses} classes it is set as 10")
+    numClasses = 10
+
+print("")
+
+for i in range(numClasses):
+    

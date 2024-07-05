7812e0fb64c82b9dd515a00ad2490cdf1d86b3d6
LegosAndStuff <masonnorberg@midco.net>
2024-07-05 17:29:56 -0500

    add theme

--- setup_term.py
+++ setup_term.py
@@ -1,5 +1,14 @@
 from datetime import datetime
+import json
 
 print("Welcome to Assignment Tracker!")
 print("Today we are going to set up your experience!")
-print("\n\n")
+print("\n")
+
+setup = {}
+
+theme = input("What theme would you like (dark or light): ")
+
+setup["theme"] = theme
+
+print(setup)

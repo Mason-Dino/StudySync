90e6c093c2891ae906ae95e170510bd29cf5edce
LegosAndStuff <masonnorberg@midco.net>
2024-07-07 11:32:02 -0500

    changed grid config

--- setup.py
+++ setup.py
@@ -23,16 +23,16 @@ class setup(customtkinter.CTk):
 
         self.side = customtkinter.CTkFrame(master=self, corner_radius=0)
         self.side.grid(row=0, rowspan=3, column=0, sticky="nswe")
-        self.side.columnconfigure(0, weight=1)
-        self.side.rowconfigure((0,1), weight=1)
+        self.side.grid_columnconfigure(0, weight=1)
+        self.side.grid_rowconfigure((0,1), weight=1)
 
         self.logo = customtkinter.CTkLabel(master=self.side, anchor="n", text="Setup", font=customtkinter.CTkFont(size=20, weight="bold"))
         self.logo.grid(row=0, column=0, padx=5, pady=5)
 
         self.questionFrame1 = customtkinter.CTkFrame(master=self)
         self.questionFrame1.grid(row=0, column=1, columnspan=3, rowspan=2, padx=5, pady=5, sticky="nswe")
-        self.questionFrame1.columnconfigure(0, weight=1)
-        self.questionFrame1.rowconfigure((0,1), weight=1)
+        self.questionFrame1.grid_columnconfigure(0, weight=1)
+        self.questionFrame1.grid_rowconfigure((0,1), weight=1)
 
         self.question = customtkinter.CTkLabel(master=self.questionFrame1, text="What mode do you want?")
         self.question.grid(row=0, column=0, padx=5, pady=5)
@@ -41,8 +41,8 @@ class setup(customtkinter.CTk):
         self.mode.grid(row=1, column=0, padx=5, pady=5)
 
         self.questionFrame2 = customtkinter.CTkFrame(master=self)
-        self.questionFrame2.columnconfigure(0, weight=1)
-        self.questionFrame2.rowconfigure((0,1), weight=1)
+        self.questionFrame2.grid_columnconfigure(0, weight=1)
+        self.questionFrame2.grid_rowconfigure((0,1), weight=1)
 
         self.question = customtkinter.CTkLabel(master=self.questionFrame2, text="What theme do you want?")
         self.question.grid(row=0, column=0, padx=5, pady=5)
@@ -51,8 +51,8 @@ class setup(customtkinter.CTk):
         self.theme.grid(row=1, column=0, padx=5, pady=5)
 
         self.questionFrame3 = customtkinter.CTkFrame(master=self)
-        self.questionFrame3.columnconfigure(0, weight=1)
-        self.questionFrame3.rowconfigure((0,1), weight=1)
+        self.questionFrame3.grid_columnconfigure(0, weight=1)
+        self.questionFrame3.grid_rowconfigure((0,1), weight=1)
 
         self.question = customtkinter.CTkLabel(master=self.questionFrame3, text="How many classes do you have?")
         self.question.grid(row=0, column=0, padx=5, pady=5)
@@ -61,8 +61,8 @@ class setup(customtkinter.CTk):
         self.numClass.grid(row=1, column=0, padx=5, pady=5)
 
         self.classFrame = customtkinter.CTkFrame(master=self)
-        self.classFrame.columnconfigure(0, weight=1)
-        self.classFrame.rowconfigure((0,1,2), weight=1)
+        self.classFrame.grid_columnconfigure(0, weight=1)
+        self.classFrame.grid_rowconfigure((0,1,2), weight=1)
 
         self.className = customtkinter.CTkEntry(master=self.classFrame, placeholder_text="What is the class name?", width=200)
         self.className.grid(row=0, column=0, padx=5, pady=5)
@@ -77,6 +77,9 @@ class setup(customtkinter.CTk):
 
         self.pageButton = customtkinter.CTkSegmentedButton(master=self, values=["Last", "Next"], command=self.page)
         self.pageButton.grid(row=2, column=1, columnspan=3, padx=5, pady=5, sticky="nswe")
+
+        self.congratsFrame = customtkinter.CTkFrame(master=self)
+        
 
         self.pageInfo = [self.questionFrame1, self.questionFrame2, self.questionFrame3, self.classFrame]
 

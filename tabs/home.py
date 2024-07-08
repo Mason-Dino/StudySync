ad119b965727fa8b6582c9aed3b5198119e2ca55
LegosAndStuff <masonnorberg@midco.net>
2024-07-07 21:38:36 -0500

    starting to work on the home page

--- tabs/home.py
+++ tabs/home.py
@@ -6,7 +6,8 @@ def home(self):
     self.content.grid(row=0, column=1, rowspan=3, columnspan=2, sticky="nsew", padx=10, pady=10)
 
     self.content.grid_columnconfigure(0, weight=1)
-    self.content.grid_rowconfigure(0, weight=1)
+    #self.content.grid_rowconfigure((), weight=1)
 
-    self.test = customtkinter.CTkLabel(master=self.content, text="Home")
-    self.test.grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
+    self.progressbar = customtkinter.CTkProgressBar(master=self.content)
+    self.progressbar.grid(row=0, column=0, columnspan=3, sticky="nsew", padx=10, pady=10)
+    self.progressbar.set(0)

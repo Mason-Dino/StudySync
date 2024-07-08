6ee2e97e7f75c381e6fe14852ffc362581686daa
LegosAndStuff <masonnorberg@midco.net>
2024-07-07 21:35:04 -0500

    made it just call home function insted of repeating code

--- StudySync.py
+++ StudySync.py
@@ -61,14 +61,7 @@ class StudySync(customtkinter.CTk):
         self.settingsButton = customtkinter.CTkButton(master=self.side, text="Settings", command=lambda:settings(self), image=self.image)
         self.settingsButton.grid(row=3, column=0, padx=15, pady=100)
 
-        self.content = customtkinter.CTkFrame(master=self)
-        self.content.grid(row=0, column=1, rowspan=3, columnspan=2, sticky="nsew", padx=10, pady=10)
-
-        self.content.grid_columnconfigure(0, weight=1)
-        self.content.grid_rowconfigure(0, weight=1)
-
-        self.test = customtkinter.CTkLabel(master=self.content, text="Home")
-        self.test.grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
+        home(self)
 
     def button_function(self):
         print("button pressed")

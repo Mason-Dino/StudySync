37beb66df624df094dd4a8a4715e30de466d1f72
LegosAndStuff <masonnorberg@midco.net>
2024-07-10 18:46:32 -0500

    display classes (no names

--- tabs/classes.py
+++ tabs/classes.py
@@ -9,8 +9,13 @@ def classes(self):
     self.content = customtkinter.CTkFrame(master=self)
     self.content.grid(row=0, column=1, rowspan=3, columnspan=2, sticky="nsew", padx=10, pady=10)
 
-    self.content.grid_columnconfigure((1,2), weight=1)
-    self.content.grid_rowconfigure((0,1,2,3,4), weight=1)
+    self.content.grid_columnconfigure(0, weight=1)
+    self.content.grid_rowconfigure(0, weight=1)
+
+    self.classFrame = customtkinter.CTkFrame(master=self.content, fg_color="transparent")
+    self.classFrame.grid(row=0, column=0, columnspan=3, sticky="nsew", padx=10, pady=10)
+
+    self.classFrame.grid_columnconfigure((0,1), weight=1)
 
     with open("setup.json", "r") as f:
         self.setupDir = json.load(f)
@@ -22,20 +27,23 @@ def classes(self):
         c = 0
         row = 0
 
-        while (c < (numClasses - 1)):
-            #print(c)
+        while (c < numClasses):
             c += 1
 
-            if c % 2 == 0:
-                self.button = customtkinter.CTkButton(master=self.content, text=f"Class {c}")
-                self.button.grid(row=row, column=1, padx=10, pady=10)
+            if c == numClasses:
+                self.button = customtkinter.CTkButton(master=self.classFrame, text=f"Class {c}")
+                self.button.grid(row=row, column=0, columnspan=2, pady=10)
+
+            elif c % 2 == 0:
+                self.button = customtkinter.CTkButton(master=self.classFrame, text=f"Class {c}")
+                self.button.grid(row=row, column=1, pady=10)
                 row += 1
 
             else:
-                self.button = customtkinter.CTkButton(master=self.content, text=f"Class {c}")
-                self.button.grid(row=row, column=0, padx=10, pady=10)
+                self.button = customtkinter.CTkButton(master=self.classFrame, text=f"Class {c}")
+                self.button.grid(row=row, column=0, pady=10)
             
-            print(f"{c} {row}")
+            #print(f"{c} {row}")
 
     #self.test = customtkinter.CTkLabel(master=self.content, text="Classes")
     #self.test.grid(row=0, column=0, sticky="nsew", padx=10, pady=10)

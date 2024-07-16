d90173e820602e77a0054c4516d495695d2e2006
LegosAndStuff <masonnorberg@midco.net>
2024-07-15 19:39:50 -0500

    working on task making screen

--- tabs/home.py
+++ tabs/home.py
@@ -1,13 +1,27 @@
 import customtkinter
-
+import datetime
 
 def home(self):
     self.content = customtkinter.CTkFrame(master=self)
     self.content.grid(row=0, column=1, rowspan=3, columnspan=2, sticky="nsew", padx=10, pady=10)
 
     self.content.grid_columnconfigure(0, weight=1)
-    #self.content.grid_rowconfigure((), weight=1)
 
     self.progressbar = customtkinter.CTkProgressBar(master=self.content)
     self.progressbar.grid(row=0, column=0, columnspan=3, sticky="nsew", padx=10, pady=10)
     self.progressbar.set(0)
+
+    self.taskName = customtkinter.CTkEntry(master=self.content, placeholder_text="Task Name")
+    self.taskName.grid(row=2, column=0, columnspan=2, sticky="nsew", padx=10, pady=10)
+
+    self.button = customtkinter.CTkButton(master=self.content, text="Add Task")
+    self.button.grid(row=2, column=2, sticky="nsew", padx=10, pady=10)
+
+    self.day = customtkinter.CTkEntry(master=self.content, placeholder_text="Day")
+    self.day.grid(row=3, column=0, sticky="nsew", padx=10, pady=10)
+
+    self.month = customtkinter.CTkEntry(master=self.content, placeholder_text="Month")
+    self.month.grid(row=3, column=1, sticky="nsew", padx=10, pady=10)
+
+    self.year = customtkinter.CTkEntry(master=self.content, placeholder_text="Year", textvariable=customtkinter.StringVar(value=datetime.datetime.now().year))
+    self.year.grid(row=3, column=2, sticky="nsew", padx=10, pady=10)

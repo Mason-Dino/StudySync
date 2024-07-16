5e183a470d4c7e60a2e0e90016b50d42ee1fee78
LegosAndStuff <masonnorberg@midco.net>
2024-07-15 19:51:52 -0500

    working on task add display

--- tabs/home.py
+++ tabs/home.py
@@ -1,11 +1,19 @@
 import customtkinter
 import datetime
+import json
 
 def home(self):
     self.content = customtkinter.CTkFrame(master=self)
     self.content.grid(row=0, column=1, rowspan=3, columnspan=2, sticky="nsew", padx=10, pady=10)
 
     self.content.grid_columnconfigure(0, weight=1)
+
+    with open("setup.json", "r") as f:
+        self.setupDir = json.load(f)
+
+    self.classNum = self.setupDir["numClasses"]
+
+    classNameValues = ["None"]
 
     self.progressbar = customtkinter.CTkProgressBar(master=self.content)
     self.progressbar.grid(row=0, column=0, columnspan=3, sticky="nsew", padx=10, pady=10)
@@ -14,8 +22,13 @@ def home(self):
     self.taskName = customtkinter.CTkEntry(master=self.content, placeholder_text="Task Name")
     self.taskName.grid(row=2, column=0, columnspan=2, sticky="nsew", padx=10, pady=10)
 
-    self.button = customtkinter.CTkButton(master=self.content, text="Add Task")
-    self.button.grid(row=2, column=2, sticky="nsew", padx=10, pady=10)
+    for i in range(self.classNum):
+        classNameValues.append(self.setupDir[f"class{i+1}"]["name"])
+
+    
+
+    self.className = customtkinter.CTkOptionMenu(master=self.content, values=classNameValues, variable=customtkinter.StringVar(value="Pick Class"))
+    self.className.grid(row=2, column=2, sticky="nsew", padx=10, pady=10)
 
     self.day = customtkinter.CTkEntry(master=self.content, placeholder_text="Day")
     self.day.grid(row=3, column=0, sticky="nsew", padx=10, pady=10)
@@ -25,3 +38,7 @@ def home(self):
 
     self.year = customtkinter.CTkEntry(master=self.content, placeholder_text="Year", textvariable=customtkinter.StringVar(value=datetime.datetime.now().year))
     self.year.grid(row=3, column=2, sticky="nsew", padx=10, pady=10)
+
+
+    self.button = customtkinter.CTkButton(master=self.content, text="Add Task")
+    self.button.grid(row=4, column=1, sticky="nsew", padx=10, pady=10)

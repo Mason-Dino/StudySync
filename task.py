4d1e7fd3977a2af1ed89782f122357ace828c3e0
LegosAndStuff <masonnorberg@midco.net>
2024-07-17 18:55:29 -0500

    working on the database

--- task.py
+++ task.py
@@ -1,19 +1,43 @@
 import customtkinter
 import sqlite3
 
-def datebase():
+def database():
     conn = sqlite3.connect('study.db')
     c = conn.cursor()
     
     c.execute("""CREATE TABLE IF NOT EXISTS tasks (
-        id integer PRIMARY KEY,
+        id text PRIMARY KEY,
         task text,
         due text,
         parentID text,
-        classID text,
+        ClassName text,
+        classID text
         )""")
     conn.commit()
     conn.close()
 
+    if __name__ == "__main__":
+        print("Database created successfully")
+
 def addMainTask(taskName, taskID, className, classID, day, month, year):
     print(taskName, taskID, className, classID, day, month, year)
+
+    conn = sqlite3.connect('study.db')
+    c = conn.cursor()
+
+    c.execute(f"INSERT INTO tasks VALUES (?, ?, ?, ?, ?, ?)", (taskID, taskName, f'{year}-{month}-{day}', "None", className, classID))
+    conn.commit()
+    conn.close()
+
+def getMainTasks():
+    conn = sqlite3.connect('study.db')
+    c = conn.cursor()
+
+    c.execute("SELECT * FROM tasks WHERE parentID = 'None'")
+    rows = c.fetchall()
+    conn.close()
+
+    return rows
+
+if __name__ == "__main__":
+    database()

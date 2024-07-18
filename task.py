04b892536f7b887f291050b581a4f21459d62dc1
LegosAndStuff <masonnorberg@midco.net>
2024-07-18 08:12:39 -0500

    add finishMainTask Function

--- task.py
+++ task.py
@@ -1,5 +1,6 @@
 import customtkinter
 import sqlite3
+import json
 
 def database():
     conn = sqlite3.connect('study.db')
@@ -45,6 +46,36 @@ def getMainTasks(organize: str = None):
     conn.close()
 
     return rows
+
+def finishMainTask(self, id):
+    conn = sqlite3.connect('study.db')
+    c = conn.cursor()
+
+    c.execute(f"DELETE FROM tasks WHERE id='{id}'")
+
+    conn.commit()
+    conn.close()
+    
+    before = self.progressbar.get()
+
+    with open("setup.json", "r") as f:
+        self.setupDir = json.load(f)
+
+    self.progressbar.step()
+    self.progressbar.step()
+
+    after = self.progressbar.get()
+    print(before, after)
+    if before > after:
+        self.setupDir["level"] += 1
+        print("level up")
+
+
+    self.setupDir["progress"] = self.progressbar.get()
+
+    with open("setup.json", "w") as f:
+        json.dump(self.setupDir, f, indent=4)
+
 
 if __name__ == "__main__":
     database()

e32abe9ee35f989563ad11398c59c33f906241d8
LegosAndStuff <masonnorberg@midco.net>
2024-07-18 20:09:14 -0500

    add plans if no classes exist

--- tabs/setting.py
+++ tabs/setting.py
@@ -8,7 +8,7 @@ def settings(self):
     overallPadyOutside = 8
     overallPadyInside = 9
 
-    self.content = customtkinter.CTkFrame(master=self)
+    self.content = customtkinter.CTkScrollableFrame(master=self)
     self.content.grid(row=0, column=1, rowspan=3, columnspan=2, sticky="nsew", padx=10, pady=10)
 
     self.content.grid_columnconfigure(0, weight=1)
@@ -16,7 +16,7 @@ def settings(self):
     with open("setup.json", "r") as f:
         self.setupDir = json.load(f)
 
-    self.appearanceFrame = customtkinter.CTkFrame(master=self.content)
+    self.appearanceFrame = customtkinter.CTkFrame(master=self.content, fg_color=["gray88", "gray19"])
     self.appearanceFrame.grid(row=0, column=0, sticky="nsew", padx=10, pady=overallPadyOutside)
     self.appearanceFrame.grid_columnconfigure((1), weight=1)
 
@@ -26,7 +26,7 @@ def settings(self):
     self.currentAppearance = customtkinter.CTkLabel(master=self.appearanceFrame, text="Appearance Mode: " + self.setupDir["mode"])
     self.currentAppearance.grid(row=0, column=1, sticky="nsew", padx=10, pady=10)
 
-    self.themeFrame = customtkinter.CTkFrame(master=self.content)
+    self.themeFrame = customtkinter.CTkFrame(master=self.content, fg_color=["gray88", "gray19"])
     self.themeFrame.grid(row=1, column=0, sticky="nsew", padx=10, pady=overallPadyInside)
     self.themeFrame.grid_columnconfigure((1), weight=1)
 
@@ -36,12 +36,15 @@ def settings(self):
     self.currentTheme = customtkinter.CTkLabel(master=self.themeFrame, text="Current Theme: " + self.setupDir["theme"])
     self.currentTheme.grid(row=0, column=1, sticky="nsew", padx=10, pady=10)
 
-    self.classEditFrame = customtkinter.CTkFrame(master=self.content)
+    self.classEditFrame = customtkinter.CTkFrame(master=self.content, fg_color=["gray88", "gray19"])
     self.classEditFrame.grid(row=2, column=0, sticky="nsew", padx=10, pady=overallPadyInside)
     self.classEditFrame.grid_columnconfigure((1), weight=1)
 
     for i in range(self.setupDir["numClasses"]):
         classes.append(self.setupDir[f"class{i+1}"]["name"])
+
+    if classes == []:
+        classes.append("No Classes Available")
 
     self.classEdit = customtkinter.CTkOptionMenu(master=self.classEditFrame, values=classes, command=lambda x: editClass(self, x), width=200)
     self.classEdit.grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
@@ -51,11 +54,19 @@ def settings(self):
 
     self.newClassType = customtkinter.CTkOptionMenu(master=self.classEditFrame, 
                                                     values=["Math", "Science", "English",  "History", "Social Studies", "World Language", "Fine Arts/Music", "Arts", "Physical Education", "Other"], 
-                                                    width=200, variable=customtkinter.StringVar(value=self.setupDir["class1"]["subject"]))
+                                                    width=200)
     self.newClassType.grid(row=1, column=0, sticky="nsew", padx=10, pady=10)
 
-    self.newInstructor = customtkinter.CTkEntry(master=self.classEditFrame, placeholder_text="Instructor Name", textvariable=customtkinter.StringVar(value=self.setupDir["class1"]["teacher"]))
+    self.newInstructor = customtkinter.CTkEntry(master=self.classEditFrame, placeholder_text="Instructor Name", textvariable=customtkinter.StringVar(value="Instructor Name"))
     self.newInstructor.grid(row=1, column=1, sticky="nsew", padx=10, pady=10)
+
+    if classes == ["No Classes Available"]:
+        self.newInstructor.configure(textvariable=customtkinter.StringVar(value="No Classes Available"))
+        self.newClassType.configure(variable=customtkinter.StringVar(value="No Classes Available"))
+
+    else:
+        self.newInstructor.configure(textvariable=customtkinter.StringVar(value=self.setupDir["class1"]["teacher"]))
+        self.newClassType.configure(variable=customtkinter.StringVar(value=self.setupDir["class1"]["subject"]))
 
     self.classEditButtons = customtkinter.CTkFrame(master=self.classEditFrame, fg_color="transparent")
     self.classEditButtons.grid(row=2, column=0, columnspan=2, sticky="nsew", padx=10, pady=10)
@@ -67,7 +78,7 @@ def settings(self):
     self.delete = customtkinter.CTkButton(master=self.classEditButtons, text="Delete Class", command=lambda: deleteClass(self, self.classEdit.get()))
     self.delete.grid(row=0, column=1, sticky="nsew", padx=10, pady=10)
 
-    self.level = customtkinter.CTkFrame(master=self.content)
+    self.level = customtkinter.CTkFrame(master=self.content, fg_color=["gray88", "gray19"])
     self.level.grid(row=3, column=0, sticky="nsew", padx=10, pady=overallPadyOutside)
     self.level.grid_columnconfigure((0,1), weight=1)
 

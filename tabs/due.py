4682455337af562390035e398e5223588c2199d0
LegosAndStuff <masonnorberg@midco.net>
2024-07-20 19:13:32 -0500

    print days of week

--- tabs/due.py
+++ tabs/due.py
@@ -1,12 +1,19 @@
 import customtkinter
+import datetime
 
 
 def due(self):
     self.content = customtkinter.CTkFrame(master=self)
     self.content.grid(row=0, column=1, rowspan=3, columnspan=2, sticky="nsew", padx=10, pady=10)
 
-    self.content.grid_columnconfigure(0, weight=1)
-    #self.content.grid_rowconfigure(0, weight=1)
+    date = datetime.datetime.today()
+    monday = date.weekday()
+    print(monday)
 
-    self.test = customtkinter.CTkLabel(master=self.content, text="Due Soon")
-    self.test.grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
+    print(f"Monday: {date - datetime.timedelta(days=monday)}")
+    print(f"Monday: {date - datetime.timedelta(days=monday - 1)}")
+    print(f"Monday: {date - datetime.timedelta(days=monday - 2)}")
+    print(f"Monday: {date - datetime.timedelta(days=monday - 3)}")
+    print(f"Monday: {date - datetime.timedelta(days=monday - 4)}")
+    print(f"Monday: {date - datetime.timedelta(days=monday - 5)}")
+    print(f"Monday: {date - datetime.timedelta(days=monday - 6)}")

f2ab36e60a77b06e32d1507c4614884d723aecb3
LegosAndStuff <masonnorberg@midco.net>
2024-07-22 11:11:45 -0500

    made getIcons function

--- icon.py
+++ icon.py
@@ -1,5 +1,20 @@
 import os
 
 def getIcons():
-    print(os.listdir("./icons"))
-    return ["test", "test123"]
+    icons = os.listdir("./icons")
+    
+    for i in range(len(icons)):
+        icons[i] = icons[i].replace(".png", "")
+
+    for i in range(len(icons)):
+        icons[i] = icons[i].replace("_", " ")
+
+    icons.remove("settings")
+    icons.remove("Other")
+
+    icons.append("Other")
+
+    return icons
+
+if __name__ == "__main__":
+    print(getIcons())

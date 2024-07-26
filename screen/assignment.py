7971eb3b9b617d172391d4b3a89162a6de1c3d7e
LegosAndStuff <masonnorberg@midco.net>
2024-07-25 20:28:54 -0500

    add due date

--- screen/assignment.py
+++ screen/assignment.py
@@ -19,7 +19,10 @@ def showAssignment(self, id):
     self.subTaskFrame = customtkinter.CTkFrame(master=self.content, corner_radius=6, fg_color=topLevel())
     self.subTaskFrame.grid(row=1, column=0, columnspan=2, sticky="nsew", padx=10, pady=7)
 
-    self.assignment = customtkinter.CTkLabel(master=self.header, text=f"{taskInfo[0][1]}", font=customtkinter.CTkFont(size=20, weight="bold"))
+    self.assignment = customtkinter.CTkLabel(master=self.header, text=f"{taskInfo[0][1]}", font=customtkinter.CTkFont(size=20, weight="bold"), width=400, anchor="w", justify="left",)
     self.assignment.grid(row=0, column=0, columnspan=2, sticky="nsew", padx=10, pady=10)
+
+    self.due = customtkinter.CTkLabel(master=self.header, text=f"Due: {taskInfo[0][3]}/{taskInfo[0][2]}/{taskInfo[0][4]}", font=customtkinter.CTkFont(size=15), width=400, anchor="w", justify="left",)
+    self.due.grid(row=1, column=0, columnspan=2, sticky="nsew", padx=10, pady=4)
 
 

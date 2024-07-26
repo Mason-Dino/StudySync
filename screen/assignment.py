0294feaf5459d2dcf94f4bdcf91e1d4c256e6e79
LegosAndStuff <masonnorberg@midco.net>
2024-07-26 12:16:36 -0500

    working on displaying subtask

--- screen/assignment.py
+++ screen/assignment.py
@@ -2,6 +2,7 @@ import customtkinter
 from task import *
 
 from themes.theme import topLevel
+from id import makeID
 
 def showAssignment(self, id):
     self.content = customtkinter.CTkFrame(master=self, corner_radius=6)
@@ -10,8 +11,10 @@ def showAssignment(self, id):
     self.content.grid_rowconfigure((1), weight=1)
 
     taskInfo = getMainTaskSingle(id)
+    subTask = getSubTasks(id)
 
-    print(taskInfo)
+    print(subTask)
+    classID = taskInfo[0][8]
 
     self.header = customtkinter.CTkFrame(master=self.content, corner_radius=6, fg_color=topLevel())
     self.header.grid(row=0, column=0, columnspan=2, sticky="nsew", padx=10, pady=7)
@@ -41,7 +44,7 @@ def showAssignment(self, id):
     self.classContent.grid(row=0, column=1, sticky="nsew", padx=10, pady=4)
 
     self.addSubTask = customtkinter.CTkButton(master=self.subTaskFrame, text="Add Sub-Task", fg_color="transparent", hover_color=["gray90", "gray21"], compound="left", anchor="w",
-                                                font=customtkinter.CTkFont(size=15), command=lambda: addSubTaskDisplay(self, id))
+                                                font=customtkinter.CTkFont(size=15), command=lambda: addSubTaskDisplay(self, id, classID, taskInfo))
     self.addSubTask.grid(row=0, column=0, sticky="nsew", padx=10, pady=6)
 
     self.edit = customtkinter.CTkButton(master=self.buttonFrame, text="Edit")
@@ -53,7 +56,7 @@ def showAssignment(self, id):
     self.complete = customtkinter.CTkButton(master=self.buttonFrame, text="Complete")
     self.complete.grid(row=0, column=1, sticky="nsew", padx=10, pady=6)
 
-def addSubTaskDisplay(self, parentid):
+def addSubTaskDisplay(self, parentID, classID, taskInfo):
     self.addSubTaskFrame = customtkinter.CTkFrame(master=self.content, corner_radius=6, fg_color=topLevel())
     self.addSubTaskFrame.grid(row=1, column=0, columnspan=2, sticky="nsew", padx=10, pady=7)
     self.addSubTaskFrame.grid_columnconfigure((0), weight=1)
@@ -61,12 +64,14 @@ def addSubTaskDisplay(self, parentid):
     self.subTaskEntry = customtkinter.CTkEntry(master=self.addSubTaskFrame, placeholder_text="Sub-Task Name (max: 30)")
     self.subTaskEntry.grid(row=0, column=0, sticky="nsew", padx=10, pady=6)
 
-    self.subTaskButton = customtkinter.CTkButton(master=self.addSubTaskFrame, text="Add Sub-Task", command=lambda: addSubTaskFunction(self, parentid))
+    self.subTaskButton = customtkinter.CTkButton(master=self.addSubTaskFrame, text="Add Sub-Task", command=lambda: addSubTaskFunction(self, parentID, classID, taskInfo))
     self.subTaskButton.grid(row=0, column=1, sticky="nsew", padx=10, pady=6)
 
-def addSubTaskFunction(self, parentid):
+def addSubTaskFunction(self, parentID, classID, taskInfo):
     subTaskName = self.subTaskEntry.get()
 
     self.addSubTaskFrame.destroy()
     self.addSubTask.grid(row=1, column=0, sticky="nsew")
-    print(parentid)
+
+    id = makeID(20)
+    addSubTask(subTaskName, id, taskInfo[0][7], taskInfo[0][8], taskInfo[0][2], taskInfo[0][3], taskInfo[0][4], parentID)

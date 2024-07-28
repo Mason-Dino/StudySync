2bb01363afde0546838cd3122ef157abc7c2fc3f
LegosAndStuff <masonnorberg@midco.net>
2024-07-28 08:36:19 -0500

    add getClassTask to classroom sreen (no display)

--- screen/classroom.py
+++ screen/classroom.py
@@ -5,6 +5,7 @@ from task import *
 
 def classroom(self, id, className):
     classInfo = getClassInfo(id)
+    task = getClassTasks(id)
 
     self.content = customtkinter.CTkFrame(master=self)
     self.content.grid(row=0, column=1, rowspan=3, columnspan=2, sticky="nsew", padx=10, pady=10)

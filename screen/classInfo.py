2bc809e497237dabfce09857b640f78d6d8a4b3f
LegosAndStuff <masonnorberg@midco.net>
2024-07-28 12:54:18 -0500

    changed the apperance of class info

--- screen/classInfo.py
+++ screen/classInfo.py
@@ -14,50 +14,42 @@ def classInfoScreen(self, id):
     self.header = customtkinter.CTkLabel(master=self.content, text=f"{classInfo['name']} Class Info", font=customtkinter.CTkFont(size=20, weight="bold"))
     self.header.grid(row=0, column=0, sticky="nsew", padx=10, pady=7)
 
-    self.classNameFrame = customtkinter.CTkFrame(master=self.content, corner_radius=6, fg_color=topLevel())
-    self.classNameFrame.grid(row=1, column=0, sticky="nsew", padx=10, pady=7)
-    self.classNameFrame.grid_columnconfigure((0,1), weight=1)
-
-    self.className = customtkinter.CTkLabel(master=self.classNameFrame, text=f"Class Name:")
-    self.className.grid(row=0, column=0, sticky="nsew", padx=10, pady=7)
-
-    self.classNameLabel = customtkinter.CTkLabel(master=self.classNameFrame, text=f"{classInfo['name']}")
-    self.classNameLabel.grid(row=0, column=1, sticky="nsew", padx=10, pady=7)
-
     self.classTeacherFrame = customtkinter.CTkFrame(master=self.content, corner_radius=6, fg_color=topLevel())
     self.classTeacherFrame.grid(row=2, column=0, sticky="nsew", padx=10, pady=7)
     self.classTeacherFrame.grid_columnconfigure((0,1), weight=1)
 
-    self.classTeacher = customtkinter.CTkLabel(master=self.classTeacherFrame, text=f"Teacher:")
-    self.classTeacher.grid(row=0, column=0, sticky="nsew", padx=10, pady=7)
+    self.className = customtkinter.CTkLabel(master=self.classTeacherFrame, text=f"Class Name:")
+    self.className.grid(row=0, column=0, sticky="nsew", padx=10, pady=7)
 
-    self.classTeacher2 = customtkinter.CTkLabel(master=self.classTeacherFrame, text=f"{classInfo['teacher']}")
-    self.classTeacher2.grid(row=0, column=1, sticky="nsew", padx=10, pady=7)
+    self.classNameLabel = customtkinter.CTkLabel(master=self.classTeacherFrame, text=f"{classInfo['name']}")
+    self.classNameLabel.grid(row=0, column=1, sticky="nsew", padx=10, pady=7)
+
+    self.classTeacher = customtkinter.CTkLabel(master=self.classTeacherFrame, text=f"Teacher:")
+    self.classTeacher.grid(row=1, column=0, sticky="nsew", padx=10, pady=7)
+
+    self.classTeacher2 = customtkinter.CTkLabel(master=self.classTeacherFrame, text=f"{classInfo['teacher']}", width=40, height=20)
+    self.classTeacher2.grid(row=1, column=1, sticky="nsew", padx=10, pady=7)
 
     self.classTeacherEmail = customtkinter.CTkLabel(master=self.classTeacherFrame, text=f"Email:")
-    self.classTeacherEmail.grid(row=1, column=0, sticky="nsew", padx=10, pady=7)
+    self.classTeacherEmail.grid(row=2, column=0, sticky="nsew", padx=10, pady=7)
 
-    self.classTeacherEmail2 = customtkinter.CTkLabel(master=self.classTeacherFrame, text=f"{classInfo['email']}")
-    self.classTeacherEmail2.grid(row=1, column=1, sticky="nsew", padx=10, pady=7)
+    self.classTeacherEmail2 = customtkinter.CTkLabel(master=self.classTeacherFrame, text=f"{classInfo['email']}", width=40, height=20)
+    self.classTeacherEmail2.grid(row=2, column=1, sticky="nsew", padx=10, pady=7)
 
-    self.displayFrame = customtkinter.CTkFrame(master=self.content, corner_radius=6, fg_color=topLevel())
-    self.displayFrame.grid(row=3, column=0, sticky="nsew", padx=10, pady=7)
-    self.displayFrame.grid_columnconfigure((0,1), weight=1)
+    self.subject = customtkinter.CTkLabel(master=self.classTeacherFrame, text=f"Subject:")
+    self.subject.grid(row=3, column=0, sticky="nsew", padx=10, pady=7)
 
-    self.subject = customtkinter.CTkLabel(master=self.displayFrame, text=f"Subject:")
-    self.subject.grid(row=0, column=0, sticky="nsew", padx=10, pady=7)
+    self.subject2 = customtkinter.CTkLabel(master=self.classTeacherFrame, text=f"{classInfo['subject']}")
+    self.subject2.grid(row=3, column=1, sticky="nsew", padx=10, pady=7)
 
-    self.subject2 = customtkinter.CTkLabel(master=self.displayFrame, text=f"{classInfo['subject']}")
-    self.subject2.grid(row=0, column=1, sticky="nsew", padx=10, pady=7)
+    self.icon = customtkinter.CTkLabel(master=self.classTeacherFrame, text=f"Icon:")
+    self.icon.grid(row=4, column=0, sticky="nsew", padx=10, pady=7)
 
-    self.icon = customtkinter.CTkLabel(master=self.displayFrame, text=f"Icon:")
-    self.icon.grid(row=1, column=0, sticky="nsew", padx=10, pady=7)
+    self.icon2 = customtkinter.CTkLabel(master=self.classTeacherFrame, image=loadIcon(classInfo['icon']), text="")
+    self.icon2.grid(row=4, column=1, sticky="nsew", padx=10, pady=7)
 
-    self.icon2 = customtkinter.CTkLabel(master=self.displayFrame, image=loadIcon(classInfo['icon']), text="")
-    self.icon2.grid(row=1, column=1, sticky="nsew", padx=10, pady=7)
+    self.color = customtkinter.CTkLabel(master=self.classTeacherFrame, text=f"Color:")
+    self.color.grid(row=5, column=0, sticky="nsew", padx=10, pady=7)
 
-    self.color = customtkinter.CTkLabel(master=self.displayFrame, text=f"Color:")
-    self.color.grid(row=2, column=0, sticky="nsew", padx=10, pady=7)
-
-    self.color2 = customtkinter.CTkFrame(master=self.displayFrame, fg_color=loadColor(classInfo['color']), width=40, height=20)
-    self.color2.grid(row=2, column=1, sticky="nsew", padx=10, pady=7)
+    self.color2 = customtkinter.CTkFrame(master=self.classTeacherFrame, fg_color=loadColor(classInfo['color']), width=40, height=20)
+    self.color2.grid(row=5, column=1, sticky="nsew", padx=10, pady=7)

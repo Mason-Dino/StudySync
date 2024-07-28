22e107c38a719939ac985e99764f6118846c7dc3
LegosAndStuff <masonnorberg@midco.net>
2024-07-28 11:49:48 -0500

    starting to work on display of class info

--- screen/classInfo.py
+++ screen/classInfo.py
@@ -5,4 +5,5 @@ from task import getClassInfo
 def classInfoScreen(self, id):
     classInfo = getClassInfo(id)
 
-    print(classInfo)
+    self.content = customtkinter.CTkFrame(master=self)
+    self.content.grid(row=0, column=1, rowspan=3, columnspan=2, sticky="nsew", padx=10, pady=10)

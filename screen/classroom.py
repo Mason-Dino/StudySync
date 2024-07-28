b82df8c0363b5079f43927d267be4e0a8e1aeccb
LegosAndStuff <masonnorberg@midco.net>
2024-07-28 09:02:30 -0500

    add button frame with edit and info buttons

--- screen/classroom.py
+++ screen/classroom.py
@@ -4,6 +4,7 @@ import subprocess
 
 from themes.theme import topLevel
 from task import *
+from screen.classAddEdit import classAddEdit
 
 def classroom(self, id, className):
     classInfo = getClassInfo(id)
@@ -21,6 +22,10 @@ def classroom(self, id, className):
     self.mainContent = customtkinter.CTkFrame(master=self.content, corner_radius=6, fg_color=topLevel())
     self.mainContent.grid(row=1, column=0, columnspan=2, sticky="nsew", padx=10, pady=7)
 
+    self.buttonFrame = customtkinter.CTkFrame(master=self.content, corner_radius=6, fg_color=topLevel())
+    self.buttonFrame.grid(row=2, column=0, columnspan=2, sticky="nsew", padx=10, pady=7)
+    self.buttonFrame.grid_columnconfigure((0,1), weight=1)
+
     self.className = customtkinter.CTkLabel(master=self.header, text=className, font=customtkinter.CTkFont(size=20, weight="bold"),
                                             anchor="w", justify="left")
     self.className.grid(row=0, column=0, sticky="nsew", padx=10, pady=7)
@@ -29,6 +34,12 @@ def classroom(self, id, className):
                                                 font=customtkinter.CTkFont(size=15), anchor="w", justify="left")
     self.classTeacher.grid(row=1, column=0, sticky="nsew", padx=10, pady=7)
     self.classTeacher.bind("<Button-1>", lambda event: teacherEmail(self, classInfo["email"]))
+
+    self.editClass = customtkinter.CTkButton(master=self.buttonFrame, text="Edit", command=lambda: classAddEdit(self, "edit", className))
+    self.editClass.grid(row=0, column=0, sticky="nsew", padx=10, pady=7)
+
+    self.infoClass = customtkinter.CTkButton(master=self.buttonFrame, text="Info", command=lambda: print("info"))
+    self.infoClass.grid(row=0, column=1, sticky="nsew", padx=10, pady=7)
 
 def teacherEmail(self, email):
     cmd='echo '+ email +'|clip'

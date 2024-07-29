f4a9a8f1c3cdcd4ecfd1c2b0dd822fd3fa62dd7e
LegosAndStuff <masonnorberg@midco.net>
2024-07-28 19:55:30 -0500

    check if hex code

--- themes/theme.py
+++ themes/theme.py
@@ -1,12 +1,22 @@
 import json
 import os
+import re
 
 def loadColor(color):
-    current = os.getcwd()
-    with open(f"{current}/themes/{color.lower()}.json", "r") as f:
-        color = json.load(f)
+    if "#" in color:
+        return color
+    
+    else:
+        current = os.getcwd()
+        with open(f"{current}/themes/{color.lower()}.json", "r") as f:
+            color = json.load(f)
 
-    return color["CTkButton"]["fg_color"]
+        return color["CTkButton"]["fg_color"]
+
+def isValidColorCode(code):
+    pattern = r"^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$"
+    match = re.match(pattern, code)
+    return bool(match)
 
 def topLevel():
     return ["gray88", "gray19"]

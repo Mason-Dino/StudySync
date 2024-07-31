1a17c22be9a85186d5e5dc37d47c313c09d2abbe
LegosAndStuff <masonnorberg@midco.net>
2024-07-31 08:19:01 -0500

    add loadLevelColor function

--- themes/theme.py
+++ themes/theme.py
@@ -24,5 +24,27 @@ def topLevel():
 def top2Level():
     return ["gray90", "gray21"]
 
+def loadLevelColor(level: int):
+    if level == 1:
+        return "#CE353C"
+    
+    if level == 2:
+        return "#E96A26"
+    
+    if level == 3:
+        return "#EBB82D"
+    
+    if level == 4:
+        return "#2FBB25"
+    
+    if level == 5:
+        with open("setup.json", "r") as f:
+            setupDir = json.load(f)
+
+        color = setupDir["theme"]
+
+        return loadColor(color)
+    
+
 if __name__ == "__main__":
     loadColor("purple")

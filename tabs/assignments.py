a6693f81596e5485f7eddd97db1e7d03ba22a0bc
LegosAndStuff <masonnorberg@midco.net>
2024-08-02 20:29:54 -0500

    capitalize types

--- tabs/assignments.py
+++ tabs/assignments.py
@@ -163,7 +163,7 @@ def makeTask(self):
     if error == False:
         importance = int(importance)
 
-        addMainTask(name, id, classOption, classID, day, month, year, subLink, ptsValue, importance, type)
+        addMainTask(name, id, classOption, classID, day, month, year, subLink, ptsValue, importance, type.capitalize())
 
         self.taskNameEntry.delete(0, "end")
         self.taskNameEntry.configure(placeholder_text="Task Name (max: 30)")

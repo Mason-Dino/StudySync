c52673971cafab8e408adbf685356c17fa31b022
LegosAndStuff <masonnorberg@midco.net>
2024-08-14 08:06:46 -0500

    add a getReleaseVersion funtion

--- version.py
+++ version.py
@@ -6,7 +6,14 @@ def getMainVersion():
     decode = test.content.decode("utf-8")
     res = json.loads(decode)
 
-    return res["mainVersion"]
+    return res["stable"]["main"]
+
+def getReleaseVersion():
+    test = requests.get("https://mason-dino.github.io/StudySync/")
+    decode = test.content.decode("utf-8")
+    res = json.loads(decode)
+
+    return res["release"]["version"]
 
 def getLittleVersion():
     test = requests.get("https://mason-dino.github.io/StudySync/")
@@ -19,7 +26,7 @@ def getUserVersion():
     with open("setup.json", "r") as f:
         setupDir = json.load(f)
 
-    return setupDir["stable"]["main"]
+    return setupDir["version"]
 
 def checkIfBeta():
     test = requests.get("https://mason-dino.github.io/StudySync/")

deff8e1f7cf0afb04a7147df64d66542e4bedb93
LegosAndStuff <masonnorberg@midco.net>
2024-08-14 06:12:31 -0500

    rename file

---
+++ tabs/studyTimer.py
@@ -0,0 +1,5 @@
+import customtkinter
+
+def sleepTimer(self):
+    self.content = customtkinter.CTkFrame(master=self, corner_radius=6)
+    self.content.grid(row=0, column=1, rowspan=3, columnspan=2, sticky="nsew", padx=10, pady=10)

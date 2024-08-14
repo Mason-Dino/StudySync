e7d799179af84e4b3c8b1a35447f5e3875e5dea3
LegosAndStuff <masonnorberg@midco.net>
2024-08-14 06:17:11 -0500

    rename funtion name

--- tabs/studyTimer.py
+++ tabs/studyTimer.py
@@ -1,5 +1,5 @@
 import customtkinter
 
-def sleepTimer(self):
+def studyTimer(self):
     self.content = customtkinter.CTkFrame(master=self, corner_radius=6)
     self.content.grid(row=0, column=1, rowspan=3, columnspan=2, sticky="nsew", padx=10, pady=10)

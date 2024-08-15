2b68f88045fed278843f4472dee4700e1bdaec97
LegosAndStuff <masonnorberg@midco.net>
2024-08-15 07:48:33 -0500

    working on design

--- tabs/studyTimer.py
+++ tabs/studyTimer.py
@@ -3,3 +3,11 @@ import customtkinter
 def studyTimer(self):
     self.content = customtkinter.CTkFrame(master=self, corner_radius=6)
     self.content.grid(row=0, column=1, rowspan=3, columnspan=2, sticky="nsew", padx=10, pady=10)
+    self.content.grid_columnconfigure(0, weight=1)
+
+    self.header = customtkinter.CTkFrame(master=self.content, corner_radius=6, fg_color="transparent")
+    self.header.grid(row=0, column=0, sticky="nsew", pady=20, padx=10)
+    self.header.grid_columnconfigure(0, weight=1)
+
+    self.timeLabel = customtkinter.CTkLabel(master=self.header, text="00:00:00", font=customtkinter.CTkFont(size=30, weight="bold"))
+    self.timeLabel.grid(row=0, column=0, sticky="nsew")

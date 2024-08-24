578db667857f0ca9d706b0875381de6fbc5d29a8
LegosAndStuff <masonnorberg@midco.net>
2024-08-24 08:24:04 -0500

    fixed hieght issue

--- tabs/studyTimer.py
+++ tabs/studyTimer.py
@@ -54,7 +54,7 @@ def studyTimer(self):
 
     self.miniContent = customtkinter.CTkFrame(master=self.content, fg_color="transparent")
     #self.miniContent.grid(row=3, column=0, sticky="nsew")
-    self.miniContent.grid_rowconfigure((0,1,2), weight=1)
+    self.miniContent.grid_rowconfigure((0), weight=1)
     self.miniContent.grid_columnconfigure((0), weight=1)
 
     self.breakFrame = customtkinter.CTkFrame(master=self.miniContent, fg_color=topLevel(), height=168)
@@ -73,9 +73,10 @@ def studyTimer(self):
     self.add10 = customtkinter.CTkButton(master=self.breakFrame, text="10 Min Add")
     self.add10.grid(row=1, column=1, sticky="nsew", pady=7, padx=10)
 
-    self.subFrame = customtkinter.CTkScrollableFrame(master=self.miniContent, corner_radius=6, fg_color=topLevel(), height=168)
+    self.subFrame = customtkinter.CTkScrollableFrame(master=self.miniContent, corner_radius=6, fg_color=topLevel(), height=100)
     self.subFrame.grid(row=1, column=0, rowspan=2, sticky="nsew", pady=10, padx=10)
     self.subFrame.grid_columnconfigure((0), weight=1)
+    self.subFrame._scrollbar.configure(height=0)
 
     self.noTasks = customtkinter.CTkLabel(master=self.subFrame, text="Waiting to confirm main task", font=customtkinter.CTkFont(size=15))
     self.noTasks.grid(row=0, column=0, sticky="nsew", pady=10, padx=10)
@@ -89,6 +90,9 @@ def studyTimer(self):
 
     self.stop = customtkinter.CTkButton(master=self.controlButtons, text="Stop", font=customtkinter.CTkFont(size=15))
     #self.stop.grid(row=0, column=2, sticky="nsew", pady=10, padx=10)
+
+def confirm(self):
+    print("hey")
 
 def test(self):
     self.miniContent.grid(row=3, column=0, sticky="nsew")

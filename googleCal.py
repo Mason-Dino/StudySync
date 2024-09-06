8ede96d82cd9df7d2e1eb73765be0097b4f4bfc9
LegosAndStuff <masonnorberg@midco.net>
2024-09-06 07:10:37 -0500

    added a bit more to the function

--- googleCal.py
+++ googleCal.py
@@ -4,7 +4,16 @@ def checkIfGoogleCal():
     with open("setup.json", "r") as f:
         setup = json.load(f)
 
-    if setup["googleCal"] == True:
-        return True
-    else:
+    
+    try:
+        if setup["googleCal"] == True:
+            return True
+        else:
+            return False
+        
+    except:
+        setup["googleCal"] = False
+        with open("setup.json", "w") as f:
+            json.dump(setup, f, indent=4)
+
         return False

fb09b0eb9738afc28fea7b2f2dd746e44eaba663
LegosAndStuff <masonnorberg@midco.net>
2024-09-06 15:45:04 -0500

    starting to work on setup screen

---
+++ screen/googleCalScreen.py
@@ -0,0 +1,8 @@
+from googleCal import *
+import customtkinter
+
+def googleCalSetup(self):
+    self.content = customtkinter.CTkFrame(master=self)
+    self.content.grid(row=0, column=1, rowspan=3, columnspan=2, sticky="nsew", padx=10, pady=10)
+
+    

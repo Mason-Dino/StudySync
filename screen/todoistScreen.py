192afe54d34bb2b0b24e2fed70b42dab50488475
LegosAndStuff <masonnorberg@midco.net>
2024-09-20 09:59:05 -0500

    starting on the setup

---
+++ screen/todoistScreen.py
@@ -0,0 +1,8 @@
+from tkinter import messagebox
+import customtkinter
+
+def todoistScreen(self):
+    self.content = customtkinter.CTkFrame(master=self, corner_radius=6)
+    self.content.grid(row=0, column=1, rowspan=3, columnspan=2, sticky="nsew", padx=10, pady=10)
+
+    
